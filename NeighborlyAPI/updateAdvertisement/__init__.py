8cfc06abec27ca9a1cb6aa49101a883f271862bc
Jesus Mira Aldao <jesus.mira1@hotmail.com>
2021-01-06 17:32:47 +0100

    Change CosmosDB connection string

--- NeighborlyAPI/updateAdvertisement/__init__.py
+++ NeighborlyAPI/updateAdvertisement/__init__.py
@@ -10,7 +10,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if request:
         try:
             # TODO: Update with appropriate MongoDB connection information
-            url = "mongodb://neighborlymongodbjmira:RdG7p7bwK0X2nCrPJeIErucJTVgxMWTg5gPxbpbf6oslgMowInzb3qFkn3tUMcyibldZVMsbqW3myYvDuL8HnA==@neighborlymongodbjmira.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"  
+            url = "mongodb://neighborlymongodbjmira:93e6tfk4JvB4yqrb1T509j9W3AOR3HmylzteSqgd2JSAVzdCqRtbHq6uyOSjryvwbZDEQoATJgCttZBFSEznFg==@neighborlymongodbjmira.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"  
             client = pymongo.MongoClient(url)
             database = client['azure']
             collection = database['advertisements']
